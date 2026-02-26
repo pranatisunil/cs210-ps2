c44933ecd76d76ab90ae2867939b701ef4dd5407
Pranati Sunil <pranati@bu.edu>
2026-02-26 16:18:50 -0500

    Refine comments in OR_FRAG assembly code
    Updated comments for OR_FRAG to clarify inputs and outputs.

--- or.s
+++ or.s
@@ -1,11 +1,14 @@
 .intel_syntax noprefix
-.section .text
 .global OR_FRAG
 
-# OR_FRAG:
-#   INPUTS:  rax = x, rbx = &y
-#   OUTPUTS: rax = x | *(&y), rbx = &y + 8
+# OR_FRAG Fragment
+# INPUTS: rax -> x
+#         rbx -> &y (the address of where in memory y is)
+# OUTPUTS: x = x bitwise or y : update rax with bitwise OR of the 8 byte quantity at the location of &y
+#          In addition, rbx should be updated to equal &y + 8
+
+.text
 OR_FRAG:
-    or  rax, QWORD PTR [rbx]
-    add rbx, 8
-    int3
+    or rax, [rbx]       # rax = rax | (value at address rbx)
+    add rbx, 8          # rbx = &y + 8
+    int3                # trap to debugger
