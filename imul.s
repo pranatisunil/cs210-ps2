3985233d70bdfc1216fa70eb227fa152e4cd251c
Pranati Sunil <pranati@bu.edu>
2026-02-26 16:19:23 -0500

    Refactor IMUL_FRAG for better readability and structure

--- imul.s
+++ imul.s
@@ -1,24 +1,27 @@
 .intel_syntax noprefix
-.section .text
-.global IMUL_FRAG
-.global IMUL_POSITIVE
-.global IMUL_NEGATIVE
+.global IMUL_FRAG, IMUL_POSITIVE, IMUL_NEGATIVE
 
+.data
+IMUL_POSITIVE:
+    .quad 0              # storage for positive y accumulations
+IMUL_NEGATIVE:
+    .quad 0              # storage for negative y accumulations
+
+.text
 IMUL_FRAG:
-    mov rcx, QWORD PTR [rbx]     # y
-    imul rax, rcx                # rax = x*y
-
-    cmp rcx, 0
-    jg  .pos
-.neg:
-    add QWORD PTR [IMUL_NEGATIVE], rcx
+    mov rcx, [rbx]       # rcx = y (load value from memory)
+    imul rax, rcx        # rax = x * y
+    
+    cmp rcx, 0           # compare y with 0
+    jle .negative        # if y <= 0, jump to negative case
+    
+    # y is positive
+    add [IMUL_POSITIVE], rcx   # add y to positive accumulator
     jmp .done
-.pos:
-    add QWORD PTR [IMUL_POSITIVE], rcx
+    
+.negative:
+    add [IMUL_NEGATIVE], rcx   # add y to negative accumulator
+    
 .done:
-    add rbx, 8
-    int3
-
-.section .data
-IMUL_POSITIVE: .quad 0
-IMUL_NEGATIVE: .quad 0
+    add rbx, 8           # rbx = &y + 8
+    int3                 # trap to debugger
