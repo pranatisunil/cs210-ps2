96a5f6d9055123674c69f23ab96f2836967ed687
Pranati Sunil <pranati@bu.edu>
2026-03-02 10:33:39 -0500

    Update and.s

--- and.s
+++ and.s
@@ -1,16 +1,13 @@
-
 .intel_syntax noprefix
+.section .text
 .global AND_FRAG
 
-# AND_FRAG Fragment
-# INPUTS: rax -> x
-#         rbx -> &y (the address of where in memory y is)
-# OUTPUTS: x = x bitwise and y : update rax with bitwise AND of rax and the 
-#                                8 byte quantity at the location of &y
-#          In addition, rbx should be updated to equal &y + 8
-
-.text
+# AND_FRAG:
+#   rax = x
+#   rbx = &y
+#   rax <- rax & QWORD PTR [rbx]
+#   rbx <- rbx + 8
 AND_FRAG:
-    and rax, [rbx]      # rax = rax & (value at address rbx)
-    add rbx, 8          # rbx = &y + 8
-    int3                # trap to debugger
+    and rax, QWORD PTR [rbx]
+    add rbx, 8
+    int3
