341f4a9e0fe4ac322a1a3f0fbe8fae8c4cd40e8e
Pranati Sunil <pranati@bu.edu>
2026-03-02 10:34:17 -0500

    Update imul.s

--- imul.s
+++ imul.s
@@ -1,27 +1,26 @@
 .intel_syntax noprefix
-.global IMUL_FRAG, IMUL_POSITIVE, IMUL_NEGATIVE
+.section .text
+.global IMUL_FRAG
+.global IMUL_POSITIVE
+.global IMUL_NEGATIVE
 
-.data
-IMUL_POSITIVE:
-    .quad 0              # storage for positive y accumulations
-IMUL_NEGATIVE:
-    .quad 0              # storage for negative y accumulations
-
-.text
 IMUL_FRAG:
-    mov rcx, [rbx]       # rcx = y (load value from memory)
-    imul rax, rcx        # rax = x * y
-    
-    cmp rcx, 0           # compare y with 0
-    jle .negative        # if y <= 0, jump to negative case
-    
-    # y is positive
-    add [IMUL_POSITIVE], rcx   # add y to positive accumulator
+    mov rcx, QWORD PTR [rbx]     # rcx = y
+    imul rax, rcx                # rax = x*y
+
+    cmp rcx, 0
+    jg  .pos
+.neg:
+    add QWORD PTR [IMUL_NEGATIVE], rcx
     jmp .done
-    
-.negative:
-    add [IMUL_NEGATIVE], rcx   # add y to negative accumulator
-    
+.pos:
+    add QWORD PTR [IMUL_POSITIVE], rcx
 .done:
-    add rbx, 8           # rbx = &y + 8
-    int3                 # trap to debugger
+    add rbx, 8
+    int3
+
+.section .data
+IMUL_POSITIVE:
+    .quad 0
+IMUL_NEGATIVE:
+    .quad 0
