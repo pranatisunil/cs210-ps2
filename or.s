c5a1db3f747a1fe1290ff72c4d2e2ae17fd8f855
Pranati Sunil <pranati@bu.edu>
2026-03-02 10:33:58 -0500

    Refactor OR_FRAG fragment for clarity
    Updated comments for clarity and syntax.

--- or.s
+++ or.s
@@ -1,14 +1,13 @@
 .intel_syntax noprefix
+.section .text
 .global OR_FRAG
 
-# OR_FRAG Fragment
-# INPUTS: rax -> x
-#         rbx -> &y (the address of where in memory y is)
-# OUTPUTS: x = x bitwise or y : update rax with bitwise OR of the 8 byte quantity at the location of &y
-#          In addition, rbx should be updated to equal &y + 8
-
-.text
+# OR_FRAG:
+#   rax = x
+#   rbx = &y
+#   rax <- rax | QWORD PTR [rbx]
+#   rbx <- rbx + 8
 OR_FRAG:
-    or rax, [rbx]       # rax = rax | (value at address rbx)
-    add rbx, 8          # rbx = &y + 8
-    int3                # trap to debugger
+    or  rax, QWORD PTR [rbx]
+    add rbx, 8
+    int3
